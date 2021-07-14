8ef46627b597a0dcbaf5ba211cb0011c38c07466
Hamood <hamoodwahab@gmail.com>
2021-07-14 20:08:34 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {lightTheme, darkTheme, GlobalStyles} from "./themes"
+import styled, { ThemeProvider } from "styled-components";
 
+const StyledApp = styled.div`
+width: 100%;
+display:flex ;
+justify-content: center;
+background-color: ${(props)=>props.theme.body};
+font-size: 50px;
+color: ${(props)=>props.theme.fontColor};
+transition-duration: 2s;
+`;
+const Button = styled.button`
+width: 200px;
+height: 50px;
+border-radius: 5px;
+color: ${(props)=>props.theme.fontColor};
+background-color: ${(props)=>props.theme.body};
+border-color: ${(props)=>props.theme.fontColor};
+transition-duration: 2s;
+margin: 25px auto;
+display: block;
+`
 function App() {
+
+const [theme, setTheme]= useState("light")
+const toggleTheme = () => {
+  theme === 'light' ? setTheme("dark" ) : setTheme('light')
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles/>
+      <StyledApp>
+       <div>Theme Toggle</div> 
+    
+     </StyledApp>
+     <Button onClick={()=> toggleTheme()}>Click to Toggle</Button>
+    </ThemeProvider>
   );
 }
 
